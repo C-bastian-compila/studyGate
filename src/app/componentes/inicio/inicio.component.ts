cbc2e4d74277f6734a13130a8598643a44d891b6
erikb <erik.becerra.a@mail.pucv.cl>
2022-07-04 06:48:37 -0400

    v6.3.1

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
@@ -7,14 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  mensajeAdminFormulario:FormGroup;
+
+  constructor(private servicioUsuario:UsuarioService, private Form:FormBuilder) {
+    this.mensajeAdminFormulario = this.Form.group({
+      mensajeAdmin5:['',]
+    });
+
+  }
+
+  tipoUsuario : boolean = false;
+
+  mensaje : string = "Esta pagina esta creada con mucho amor para todos ustedes y aun esta en progreso. Esperamos su comprension y paciencia."
 
   ngOnInit(): void {
+
     window.scroll({
       top: 0, 
       left: 0, 
       behavior: 'smooth'
     })
+
+    if(this.servicioUsuario.tipoUsuarioActual() == "admin"){
+      this.tipoUsuario = true;
+      this.mensajeAdminFormulario.setValue({mensajeAdmin5:this.mensaje});
+    }
+    else{
+      this.tipoUsuario = false;
+      this.mensajeAdminFormulario.setValue({mensajeAdmin5:this.mensaje});
+    }
   }
 
+  // guardarMensaje(){
+  //   this.mensajeAdminFormulario.setValue({mensajeAdmin5:this.mensaje});
+  // }
 }
